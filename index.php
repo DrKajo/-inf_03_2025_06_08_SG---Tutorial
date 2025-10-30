922bb08aaa9e2290200f486b9ef09c3fa1047b8f
DrKajo <bartosz.rodak@zsz.bobowa.pl>
2025-10-30 12:24:46 +0100

    początek zadania

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header><h1>Poznaj smoki!</h1></header>
+    <nav>
+        <div class="bloki"><h1>Baza</h1></div>
+        <div class="bloki"><h1>Opisy</h1></div>
+        <div class="bloki"><h1>Galeria</h1></div>
+    </nav>
+    <main>
+        <h3>Baza Smoków</h3>
+        <form>
+            <select>
+                <option>Smok wielki</option>
+                <option>Smok łaciaty</option>
+                <option>Smok zielony</option>
+            </select>
+        </form>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
